b5dbaa67044dd9041d795470f1f2a18a00108ebe
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-07 15:38:22 +0530

    Learn terraform

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "ec2" {
   ami= "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0ca9150f91b0e8e5d]
+  vpc_security_group_ids = ["sg-0ca9150f91b0e8e5d"]
   tags = {
     Name="test"
   }
 
 }
+
