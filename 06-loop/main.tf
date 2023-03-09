bacbb44a0b5aabcacb0e4cc38121be8e9d531db9
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-09 16:20:06 +0530

    created loop

--- 06-loop/main.tf
+++ 06-loop/main.tf
@@ -1,7 +1,14 @@
 variable "d1" {
   default = 5
 }
-
 resource "null_resource" "nothing" {
+  count = var.d1
+}
 
+variable "d2" {
+  default = [0, 1, 2, 3, 4]
+}
+
+resource "null_resource" "d2" {
+  count = length(var.d2)
 }
