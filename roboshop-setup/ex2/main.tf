1e5ea78453dcce10623154df62f7cb738d9755ee
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-09 15:59:47 +0530

    created loop

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,13 +1,34 @@
-data "aws_ami" "amiii" {
+#data "aws_ami" "ami" {
+ # most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+ # owners      = ["084055491950"]
+#}
+
+
+#resource "aws_instance" "frontend" {
+ # count                  = 5
+ # ami                    = data.aws_ami.ami.image_id
+ # instance_type          = "t3.micro"
+ # vpc_security_group_ids = ["sg-0ca9150f91b0e8e5d"]
+#}
+data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["084055491950"]
+  owners      = ["973714476881"]
 }
 
 
 resource "aws_instance" "frontend" {
-  count                  = 5
-  ami                    = data.aws_ami.amiii.image_id
+  count                  = length(var.instances)
+  ami                    = data.aws_ami.ami.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0ca9150f91b0e8e5d"]
+}
+
+variable "instances" {
+  default = ["cart", "catalogue", "user", "payment", "shipping"]
+}
+
+output "public-ip" {
+  value = aws_instance.frontend.*.public_ip
 }
