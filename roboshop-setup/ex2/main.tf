bae0151d38a6b8033448a0fac81e00f880d9d7cc
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-09 15:44:34 +0530

    created count

---
+++ roboshop-setup/ex2/main.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["084055491950"]
+}
+
+
+resource "aws_instance" "frontend" {
+  count                  = 5
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0ca9150f91b0e8e5d"]
+}
