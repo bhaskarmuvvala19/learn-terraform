33fffb032e99070b334884cf7f17860ad0733625
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-10 11:22:02 +0530

    created modules for roboshop ex4

---
+++ roboshop-setup/ex4/ec2/main.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = ["sg-0ca9150f91b0e8e5d"]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "${var.component}-dev.devopswithbhaskar.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
